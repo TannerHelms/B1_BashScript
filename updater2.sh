1271505569bd3a6cfb47fdd4567819b834dfc0fe
TannerHelms <tannerhelms01@gmail.com>
2018-09-07 08:11:41 -0600

    Add files via upload

---
+++ updater2.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+clear
+echo Welcome to my second BASH script
+echo What is your name?
+read name
+clear
+echo
+read -p "Hello there $name glad to work with you. Press [enter] to continue"
+
+x=7
+y=9
+
+echo x = $x, y = $y
+
